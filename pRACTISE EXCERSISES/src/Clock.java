09a2d33869bb33260e1247361daf9db4210adf5a
prolu <prolu@192.168.0.40>
2020-10-14 11:42:27 +0200

    clock class work in progres

--- pRACTISE EXCERSISES/src/Clock.java
+++ pRACTISE EXCERSISES/src/Clock.java
@@ -10,12 +10,32 @@ public Clock(int h, int m, int s)
 	this.minute = m;
 	this.second = s;
 }
+public void setSeconds(int second) {
+	this.second = second;
+}
+public void setMinute(int minute) {
+	this.minute = minute;
+}
+public void setHour(int hour) {
+	this.hour = hour;
+}
+public int getSecond() {
+	return this.second;
+}
+public int getMinute() {
+	return this.minute;
+}
+public
 public Clock(int totalTimeInSec) 
 {
 	this.hour = totalTimeInSec/3600;
 	this.minute = ((totalTimeInSec % 3600)/60);
 	this.second = totalTimeInSec%60;
 
+}
+public int timeInSeconds(Clock time, Clock time2) {
+	int tinsec = 
+	
 }
 public String toString() {
 	return "Time: " + hour + ":" + minute + ":" + second;
